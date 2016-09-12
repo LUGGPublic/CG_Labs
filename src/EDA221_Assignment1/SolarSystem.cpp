LUGGPublic/CG_Labs

commit 42ee685a63e03bc086cbeceadf7c68ae471e3e27

Remove templated variables as VS in the labs does not support them

src/EDA221_Assignment1/SolarSystem.cpp
@@ -34,9 +34,9 @@
 
 namespace config
 {
-	template<typename T> static constexpr T msaa_rate    = static_cast<T>(   1u);
-	template<typename T> static constexpr T resolution_x = static_cast<T>(1600u);
-	template<typename T> static constexpr T resolution_y = static_cast<T>( 900u);
+	constexpr float msaa_rate    =    1u;
+	constexpr float resolution_x = 1600u;
+	constexpr float resolution_y =  900u;
 }
 
 enum class shader_bindings {
@@ -280,8 +280,8 @@ SolarSystem::SolarSystem()
 {
 	Log::View::Init();
 
-	window = Window::Create("EDA221: Assignment 1", config::resolution_x<unsigned int>,
-	                        config::resolution_y<unsigned int>, config::msaa_rate<unsigned int>, false);
+	window = Window::Create("EDA221: Assignment 1", config::resolution_x,
+	                        config::resolution_y, config::msaa_rate, false);
 	inputHandler = new InputHandler();
 	window->SetInputHandler(inputHandler);
 
@@ -307,8 +307,8 @@ void SolarSystem::run()
 		return;
 
 	// Set up the camera
-	FPSCameraf mCamera(bonobo::pi<float> / 4.0f,
-	                   config::resolution_x<float> / config::resolution_y<float>,
+	FPSCameraf mCamera(bonobo::pi / 4.0f,
+	                   static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
 	                   0.01f, 1000.0f);
 	mCamera.mWorld.SetTranslate(glm::vec3(0.0f, 0.0f, 6.0f));
 	mCamera.mMouseSensitivity = 0.003f;

src/core/utils.h
@@ -23,8 +23,6 @@ minElementIndex(glm::tvec3<T, P> const& vector)
 
 
 namespace bonobo {
-	template<typename T>
-	constexpr T pi = T(3.1415926535897932385);
-	template<typename T>
-	constexpr T two_pi = T(2.0 * pi<double>);
+	constexpr float pi = 3.1415926535897932385f;
+	constexpr float two_pi = 2.0f * pi;
 }

commit 816d644dbb3771d3644359b95ae93f5dcdcb1e68

EDA221_Assignment1: Set proper type for window's parameters
Fixes: 42ee685a63e03bc086cbeceadf7c68ae471e3e27 ("Remove templated
variables as VS in the labs does not support them")

src/EDA221_Assignment1/SolarSystem.cpp
@@ -34,9 +34,9 @@
 
 namespace config
 {
-	constexpr float msaa_rate    =    1u;
-	constexpr float resolution_x = 1600u;
-	constexpr float resolution_y =  900u;
+	constexpr unsigned int msaa_rate    =    1u;
+	constexpr unsigned int resolution_x = 1600u;
+	constexpr unsigned int resolution_y =  900u;
 }
 
 enum class shader_bindings {