aa1b3af1a7134163af7bd3b1ba1716d6288f7fe0
Pierre Moreau <pierre.moreau@cs.lth.se>
2016-09-27 09:31:48 +0200

    EDA221: Add stubs for assignments 4 and 5

---
+++ src/EDA221/assignment4.cpp
@@ -0,0 +1,7 @@
+#include "assignment4.hpp"
+
+int main()
+{
+	//! \todo Implement assignment 4
+	return 1;
+}
---
+++ src/EDA221/assignment5.cpp
@@ -0,0 +1,7 @@
+#include "assignment5.hpp"
+
+int main()
+{
+	//! \todo Implement assignment 5
+	return 1;
+}
