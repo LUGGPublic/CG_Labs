4ccdd25280b1ac3b4ade648554406e685a9b1e26
Pierre Moreau <pierre.moreau@cs.lth.se>
2018-08-07 21:25:03 +0200

    Rewrite dependencies handling
    Dependencies are now cloned and built in the source folder instead of build
    folder, to avoid reclone and rebuild when switching between Debug and Release.
    Also force the dependencies to be built in Release mode.
    The dependencies handling was rewritten to be more maintainable and do
    everything on the first CMake run.
    
    Fixes #25
    Fixes #37

---
+++ CMake/InstallGLFW.cmake
@@ -0,0 +1,63 @@
+find_package (glfw3 QUIET ${LUGGCGL_GLFW_MIN_VERSION})
+if (NOT glfw3_FOUND)
+	set (glfw_SOURCE_DIR ${FETCHCONTENT_BASE_DIR}/glfw-source)
+	set (glfw_BINARY_DIR ${FETCHCONTENT_BASE_DIR}/glfw-build)
+
+	if (NOT EXISTS ${glfw_SOURCE_DIR})
+		message (STATUS "Cloning glfw…")
+		execute_process (
+			COMMAND ${GIT_EXECUTABLE} clone --depth=1
+			                                -b ${LUGGCGL_GLFW_MIN_VERSION}
+							https://github.com/glfw/glfw.git
+							${glfw_SOURCE_DIR}
+			OUTPUT_QUIET
+			ERROR_VARIABLE stderr
+			RESULT_VARIABLE result
+			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/dependencies
+		)
+		if (result)
+			message (FATAL_ERROR "Failed to clone glfw: ${result}\n"
+			                     "Error output: ${stderr}")
+		endif ()
+
+		set (glfw_INSTALL_DIR ${FETCHCONTENT_BASE_DIR}/glfw-install)
+		file (MAKE_DIRECTORY ${glfw_BINARY_DIR})
+		file (MAKE_DIRECTORY ${glfw_INSTALL_DIR})
+
+		message (STATUS "Setting up CMake for glfw…")
+		execute_process (
+			COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}"
+			                         -DGLFW_BUILD_DOCS=OFF
+			                         -DGLFW_BUILD_TESTS=OFF
+			                         -DGLFW_BUILD_EXAMPLES=OFF
+			                         -DCMAKE_INSTALL_PREFIX=${glfw_INSTALL_DIR}
+			                         ${glfw_SOURCE_DIR}
+			OUTPUT_QUIET
+			ERROR_VARIABLE stderr
+			RESULT_VARIABLE result
+			WORKING_DIRECTORY ${glfw_BINARY_DIR}
+		)
+		if (result)
+			message (FATAL_ERROR "CMake setup for glfw failed: ${result}\n"
+			                     "Error output: ${stderr}")
+		endif ()
+
+		message (STATUS "Building and installing glfw…")
+		execute_process (
+			COMMAND ${CMAKE_COMMAND} --build ${glfw_BINARY_DIR}
+			                         --config Release
+			                         --target install
+			OUTPUT_QUIET
+			ERROR_VARIABLE stderr
+			RESULT_VARIABLE result
+		)
+		if (result)
+			message (FATAL_ERROR "Build step for glfw failed: ${result}\n"
+			                     "Error output: ${stderr}")
+		endif ()
+
+		list (APPEND CMAKE_PREFIX_PATH ${glfw_INSTALL_DIR}/lib/cmake)
+
+		set (glfw_INSTALL_DIR)
+	endif ()
+endif ()
---
+++ CMake/InstallGLM.cmake
@@ -0,0 +1,61 @@
+find_package (glm QUIET ${LUGGCGL_GLM_MIN_VERSION})
+if (NOT glm_FOUND)
+	set (glm_SOURCE_DIR ${FETCHCONTENT_BASE_DIR}/glm-source)
+	set (glm_BINARY_DIR ${FETCHCONTENT_BASE_DIR}/glm-build)
+
+	if (NOT EXISTS ${glm_SOURCE_DIR})
+		message (STATUS "Cloning glm…")
+		execute_process (
+			COMMAND ${GIT_EXECUTABLE} clone --depth=1
+			                                -b ${LUGGCGL_GLM_MIN_VERSION}
+							https://github.com/g-truc/glm.git
+							${glm_SOURCE_DIR}
+			OUTPUT_QUIET
+			ERROR_VARIABLE stderr
+			RESULT_VARIABLE result
+			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/dependencies
+		)
+		if (result)
+			message (FATAL_ERROR "Failed to clone glm: ${result}\n"
+			                     "Error output: ${stderr}")
+		endif ()
+
+
+		set (glm_INSTALL_DIR ${FETCHCONTENT_BASE_DIR}/glm-install)
+		file (MAKE_DIRECTORY ${glm_BINARY_DIR})
+		file (MAKE_DIRECTORY ${glm_INSTALL_DIR})
+
+		message (STATUS "Setting up CMake for glm…")
+		execute_process (
+			COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}"
+			                         -DCMAKE_INSTALL_PREFIX=${glm_INSTALL_DIR}
+			                         ${glm_SOURCE_DIR}
+			OUTPUT_QUIET
+			ERROR_VARIABLE stderr
+			RESULT_VARIABLE result
+			WORKING_DIRECTORY ${glm_BINARY_DIR}
+		)
+		if (result)
+			message (FATAL_ERROR "CMake setup for glm failed: ${result}\n"
+			                     "Error output: ${stderr}")
+		endif ()
+
+		message (STATUS "Building and installing glm…")
+		execute_process (
+			COMMAND ${CMAKE_COMMAND} --build ${glm_BINARY_DIR}
+			                         --config Release
+			                         --target install
+			OUTPUT_QUIET
+			ERROR_VARIABLE stderr
+			RESULT_VARIABLE result
+		)
+		if (result)
+			message (FATAL_ERROR "Build step for glm failed: ${result}\n"
+			                     "Error output: ${stderr}")
+		endif ()
+
+		list (APPEND CMAKE_PREFIX_PATH ${glm_INSTALL_DIR}/lib/cmake)
+
+		set (glm_INSTALL_DIR)
+	endif ()
+endif ()
