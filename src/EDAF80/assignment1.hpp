c067ed8e6f50fcb4d890d3d26b7d414de451556b
Pierre Moreau <pierre.moreau@cs.lth.se>
2020-09-03 14:24:33 +0200

    Do not make attributes constant in (Spin|Orbit)Configuration
    This prevented the use of the assignment operator.

--- src/EDAF80/assignment1.hpp
+++ src/EDAF80/assignment1.hpp
@@ -2,13 +2,13 @@
 
 struct SpinConfiguration
 {
-	float const inclination{0.0f}; // in rad
-	float const speed{0.0f};       // in rad/s
+	float inclination{0.0f}; // in rad
+	float speed{0.0f};       // in rad/s
 };
 
 struct OrbitConfiguration
 {
-	float const radius{0.0f};
-	float const inclination{0.0f}; // in rad
-	float const speed{0.0f};       // in rad/s
+	float radius{0.0f};
+	float inclination{0.0f}; // in rad
+	float speed{0.0f};       // in rad/s
 };
