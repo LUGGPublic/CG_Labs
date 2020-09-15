LUGGPublic/CG_Labs

commit 1b912853f934bdc586fb18c1a90a2828f96f946a

CMake: Fix the version of stb and tinyfiledialogs
Rather than always selecting the latest master, use the current latest
commit on master to ensure that everything works as expected (and should
hopefully speed up a bit CMake re-runs as it no longer needs to check if
anything new was committed to those repositories).

CMake/InstallSTB.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare (
 	stb
 	GIT_REPOSITORY [[https://github.com/nothings/stb]]
-	GIT_TAG [[origin/master]]
+	GIT_TAG [[b42009b3b9d4ca35bc703f5310eedc74f584be58]]
 	GIT_SHALLOW ON
 )
 

CMake/InstallTinyFileDialogs.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare (
 	tinyfiledialogs
 	GIT_REPOSITORY [[https://git.code.sf.net/p/tinyfiledialogs/code]]
-	GIT_TAG [[origin/master]]
+	GIT_TAG [[fb228c5bcd7d0574d08b71b4fae84676328addaf]]
 	GIT_SHALLOW ON
 )
 

commit 98f8c3d788d61756abda904e8e9f88f5585a5864

CMake: No shallow clones for stb and tinyfiledialogs
Git shallow clones are not compatible with checking out a specific
commit hash (but are compatible with branches or tags) which is what we
were doing for stb and tinyfiledialogs as those projects do not tag
their releases.

Fixes: 1b91285 ("CMake: Fix the version of stb and tinyfiledialogs")

CMake/InstallSTB.cmake
@@ -2,7 +2,6 @@ FetchContent_Declare (
 	stb
 	GIT_REPOSITORY [[https://github.com/nothings/stb]]
 	GIT_TAG [[b42009b3b9d4ca35bc703f5310eedc74f584be58]]
-	GIT_SHALLOW ON
 )
 
 FetchContent_GetProperties (stb)

CMake/InstallTinyFileDialogs.cmake
@@ -2,7 +2,6 @@ FetchContent_Declare (
 	tinyfiledialogs
 	GIT_REPOSITORY [[https://git.code.sf.net/p/tinyfiledialogs/code]]
 	GIT_TAG [[fb228c5bcd7d0574d08b71b4fae84676328addaf]]
-	GIT_SHALLOW ON
 )
 
 FetchContent_GetProperties (tinyfiledialogs)