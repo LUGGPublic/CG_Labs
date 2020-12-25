2def0d2b6238c58984ec8b6261991bef35e19fe2
Pierre Moreau <pierre.moreau@cs.lth.se>
2020-12-25 20:16:28 +0100

    EDAN35/Lab2: Fix rendering of elements in the shadow maps

--- src/EDAN35/assignment2.cpp
+++ src/EDAN35/assignment2.cpp
@@ -353,7 +353,7 @@ edan35::Assignment2::run()
 				GLStateInspection::CaptureSnapshot("Shadow Map Generation");
 
 				for (auto const& element : sponza_elements)
-					element.render(light_matrix, glm::mat4(1.0f), fill_shadowmap_shader, set_uniforms);
+					element.render(light_matrix, element.get_transform().GetMatrix(), fill_shadowmap_shader, set_uniforms);
 				if (utils::opengl::debug::isSupported())
 				{
 					glPopDebugGroup();
