14ccb279abbc7474148ff68a316ad66a546e2908
Pierre Moreau <pierre.moreau@cs.lth.se>
2021-08-27 13:40:15 +0200

    EDAF80/Lab1: Rework the base code
    The `CelestialBody` class is included from the beginning, and the
    initial setup was modified to make it easier to test and debug issues as
    progressing through the different steps.

---
+++ src/EDAF80/CelestialBody.cpp
@@ -0,0 +1,82 @@
+#include "CelestialBody.hpp"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/trigonometric.hpp>
+
+#include "core/Log.h"
+
+CelestialBody::CelestialBody(bonobo::mesh_data const& shape,
+                             GLuint const* program,
+                             GLuint diffuse_texture_id)
+{
+	_body.node.set_geometry(shape);
+	_body.node.add_texture("diffuse_texture", diffuse_texture_id, GL_TEXTURE_2D);
+	_body.node.set_program(program);
+}
+
+glm::mat4 CelestialBody::render(std::chrono::microseconds elapsed_time,
+                                glm::mat4 const& view_projection,
+                                glm::mat4 const& parent_transform)
+{
+	// Convert the duration from microseconds to seconds.
+	auto const elapsed_time_s = std::chrono::duration<float>(elapsed_time).count();
+	// If a different ratio was needed, for example a duration in
+	// milliseconds, the following would have been used:
+	// auto const elapsed_time_ms = std::chrono::duration<float, std::milli>(elapsed_time).count();
+
+	_body.spin.rotation_angle = -glm::half_pi<float>() / 2.0f;
+
+	glm::mat4 world = parent_transform;
+
+	// Note: The second argument of `node::render()` is supposed to be the
+	// parent transform of the node, not the whole world matrix, as the
+	// node internally manages its local transforms. However in our case we
+	// manage all the local transforms ourselves, so the internal transform
+	// of the node is just the identity matrix and we can forward the whole
+	// world matrix.
+	_body.node.render(view_projection, world);
+
+	return parent_transform;
+}
+
+void CelestialBody::add_child(CelestialBody* child)
+{
+	_children.push_back(child);
+}
+
+std::vector<CelestialBody*> const& CelestialBody::get_children() const
+{
+	return _children;
+}
+
+void CelestialBody::set_orbit(OrbitConfiguration const& configuration)
+{
+	_body.orbit.radius = configuration.radius;
+	_body.orbit.inclination = configuration.inclination;
+	_body.orbit.speed = configuration.speed;
+	_body.orbit.rotation_angle = 0.0f;
+}
+
+void CelestialBody::set_scale(glm::vec3 const& scale)
+{
+	_body.scale = scale;
+}
+
+void CelestialBody::set_spin(SpinConfiguration const& configuration)
+{
+	_body.spin.axial_tilt = configuration.axial_tilt;
+	_body.spin.speed = configuration.speed;
+	_body.spin.rotation_angle = 0.0f;
+}
+
+void CelestialBody::set_ring(bonobo::mesh_data const& shape,
+                             GLuint const* program,
+                             GLuint diffuse_texture_id,
+                             glm::vec2 const& scale)
+{
+	_ring.node.set_geometry(shape);
+	_ring.node.add_texture("diffuse_texture", diffuse_texture_id, GL_TEXTURE_2D);
+	_ring.node.set_program(program);
+
+	_ring.scale = scale;
+}
