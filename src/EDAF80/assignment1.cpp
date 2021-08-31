LUGGPublic/CG_Labs

commit 58678f312f9777e609ffb0a4e3439ed99934ded0

Add shortcuts for hiding logs and GUI
Press F2 to hide/show the GUI (logs included), or F3 to hide/show only
the logs.

Fixes #33

src/EDAF80/assignment1.cpp
@@ -106,6 +106,9 @@ edaf80::Assignment1::run()
 	double nowTime, lastTime = GetTimeSeconds();
 	double fpsNextTick = lastTime + 1.0;
 
+	bool show_logs = true;
+	bool show_gui = true;
+
 	while (!glfwWindowShouldClose(window->GetGLFW_Window())) {
 		nowTime = GetTimeSeconds();
 		ddeltatime = nowTime - lastTime;
@@ -121,6 +124,11 @@ edaf80::Assignment1::run()
 		glfwPollEvents();
 		inputHandler->Advance();
 		mCamera.Update(ddeltatime, *inputHandler);
+
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
+			show_logs = !show_logs;
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED)
+			show_gui = !show_gui;
 
 		ImGui_ImplGlfwGL3_NewFrame();
 
@@ -163,8 +171,10 @@ edaf80::Assignment1::run()
 			}
 		} while (!node_stack.empty());
 
-		Log::View::Render();
-		ImGui::Render();
+		if (show_logs)
+			Log::View::Render();
+		if (show_gui)
+			ImGui::Render();
 
 		window->Swap();
 		lastTime = nowTime;

src/EDAF80/assignment2.cpp
@@ -136,6 +136,9 @@ edaf80::Assignment2::run()
 	double nowTime, lastTime = GetTimeSeconds();
 	double fpsNextTick = lastTime + 1.0;
 
+	bool show_logs = true;
+	bool show_gui = true;
+
 	while (!glfwWindowShouldClose(window->GetGLFW_Window())) {
 		nowTime = GetTimeSeconds();
 		ddeltatime = nowTime - lastTime;
@@ -151,6 +154,11 @@ edaf80::Assignment2::run()
 		glfwPollEvents();
 		inputHandler->Advance();
 		mCamera.Update(ddeltatime, *inputHandler);
+
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
+			show_logs = !show_logs;
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED)
+			show_gui = !show_gui;
 
 		ImGui_ImplGlfwGL3_NewFrame();
 
@@ -205,8 +213,10 @@ edaf80::Assignment2::run()
 		ImGui::End();
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		Log::View::Render();
-		ImGui::Render();
+		if (show_logs)
+			Log::View::Render();
+		if (show_gui)
+			ImGui::Render();
 
 		window->Swap();
 		lastTime = nowTime;

src/EDAF80/assignment3.cpp
@@ -146,6 +146,9 @@ edaf80::Assignment3::run()
 	double nowTime, lastTime = GetTimeMilliseconds();
 	double fpsNextTick = lastTime + 1000.0;
 
+	bool show_logs = true;
+	bool show_gui = true;
+
 	while (!glfwWindowShouldClose(window->GetGLFW_Window())) {
 		nowTime = GetTimeMilliseconds();
 		ddeltatime = nowTime - lastTime;
@@ -181,6 +184,10 @@ edaf80::Assignment3::run()
 		}
 		if (inputHandler->GetKeycodeState(GLFW_KEY_R) & JUST_PRESSED) {
 			reload_shaders();
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
+			show_logs = !show_logs;
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED)
+			show_gui = !show_gui;
 		}
 		switch (polygon_mode) {
 			case polygon_mode_t::fill:
@@ -206,8 +213,6 @@ edaf80::Assignment3::run()
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-		Log::View::Render();
-
 		bool opened = ImGui::Begin("Scene Control", &opened, ImVec2(300, 100), -1.0f, 0);
 		if (opened) {
 			ImGui::ColorEdit3("Ambient", glm::value_ptr(ambient));
@@ -223,7 +228,10 @@ edaf80::Assignment3::run()
 			ImGui::Text("%.3f ms", ddeltatime);
 		ImGui::End();
 
-		ImGui::Render();
+		if (show_logs)
+			Log::View::Render();
+		if (show_gui)
+			ImGui::Render();
 
 		window->Swap();
 		lastTime = nowTime;

src/EDAF80/assignment4.cpp
@@ -87,6 +87,9 @@ edaf80::Assignment4::run()
 	double nowTime, lastTime = GetTimeMilliseconds();
 	double fpsNextTick = lastTime + 1000.0;
 
+	bool show_logs = true;
+	bool show_gui = true;
+
 	while (!glfwWindowShouldClose(window->GetGLFW_Window())) {
 		nowTime = GetTimeMilliseconds();
 		ddeltatime = nowTime - lastTime;
@@ -102,6 +105,11 @@ edaf80::Assignment4::run()
 		glfwPollEvents();
 		inputHandler->Advance();
 		mCamera.Update(ddeltatime, *inputHandler);
+
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
+			show_logs = !show_logs;
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED)
+			show_gui = !show_gui;
 
 		ImGui_ImplGlfwGL3_NewFrame();
 
@@ -125,14 +133,15 @@ edaf80::Assignment4::run()
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-		Log::View::Render();
-
 		//
 		// Todo: If you want a custom ImGUI window, you can set it up
 		//       here
 		//
 
-		ImGui::Render();
+		if (show_logs)
+			Log::View::Render();
+		if (show_gui)
+			ImGui::Render();
 
 		window->Swap();
 		lastTime = nowTime;

src/EDAF80/assignment5.cpp
@@ -87,6 +87,9 @@ edaf80::Assignment5::run()
 	double nowTime, lastTime = GetTimeMilliseconds();
 	double fpsNextTick = lastTime + 1000.0;
 
+	bool show_logs = true;
+	bool show_gui = true;
+
 	while (!glfwWindowShouldClose(window->GetGLFW_Window())) {
 		nowTime = GetTimeMilliseconds();
 		ddeltatime = nowTime - lastTime;
@@ -102,6 +105,11 @@ edaf80::Assignment5::run()
 		glfwPollEvents();
 		inputHandler->Advance();
 		mCamera.Update(ddeltatime, *inputHandler);
+
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
+			show_logs = !show_logs;
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED)
+			show_gui = !show_gui;
 
 		ImGui_ImplGlfwGL3_NewFrame();
 
@@ -125,14 +133,15 @@ edaf80::Assignment5::run()
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-		Log::View::Render();
-
 		//
 		// Todo: If you want a custom ImGUI window, you can set it up
 		//       here
 		//
 
-		ImGui::Render();
+		if (show_logs)
+			Log::View::Render();
+		if (show_gui)
+			ImGui::Render();
 
 		window->Swap();
 		lastTime = nowTime;

src/EDAN35/assignment2.cpp
@@ -241,6 +241,9 @@ edan35::Assignment2::run()
 	double fpsNextTick = lastTime + 1000.0;
 	bool show_textures = true;
 
+	bool show_logs = true;
+	bool show_gui = true;
+
 	while (!glfwWindowShouldClose(window->GetGLFW_Window())) {
 		nowTime = GetTimeMilliseconds();
 		ddeltatime = nowTime - lastTime;
@@ -258,6 +261,11 @@ edan35::Assignment2::run()
 		glfwPollEvents();
 		inputHandler->Advance();
 		mCamera.Update(ddeltatime, *inputHandler);
+
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
+			show_logs = !show_logs;
+		if (inputHandler->GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED)
+			show_gui = !show_gui;
 
 		ImGui_ImplGlfwGL3_NewFrame();
 
@@ -425,7 +433,6 @@ edan35::Assignment2::run()
 		glViewport(0, 0, window_size.x, window_size.y);
 
 		GLStateInspection::View::Render();
-		Log::View::Render();
 
 		bool opened = ImGui::Begin("Render Time", nullptr, ImVec2(120, 50), -1.0f, 0);
 		if (opened)
@@ -440,7 +447,10 @@ edan35::Assignment2::run()
 		}
 		ImGui::End();
 
-		ImGui::Render();
+		if (show_logs)
+			Log::View::Render();
+		if (show_gui)
+			ImGui::Render();
 
 		window->Swap();
 		lastTime = nowTime;

commit 4a7513fa1021dda8ac4d2f413369dac67eaefafc

Always call ImGui::Render() even when hiding the GUI
It is only the drawing of the GUI which should be disabled.

Fixes: 58678f31 ("Add shortcuts for hiding logs and GUI")

src/EDAF80/assignment1.cpp
@@ -250,8 +250,7 @@ int main()
 		//
 		if (show_logs)
 			Log::View::Render();
-		if (show_gui)
-			window_manager.RenderImGuiFrame();
+		window_manager.RenderImGuiFrame(show_gui);
 
 
 		//

src/EDAF80/assignment2.cpp
@@ -240,8 +240,7 @@ edaf80::Assignment2::run()
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		if (show_logs)
 			Log::View::Render();
-		if (show_gui)
-			mWindowManager.RenderImGuiFrame();
+		mWindowManager.RenderImGuiFrame(show_gui);
 
 		glfwSwapBuffers(window);
 	}

src/EDAF80/assignment3.cpp
@@ -220,8 +220,7 @@ edaf80::Assignment3::run()
 
 		if (show_logs)
 			Log::View::Render();
-		if (show_gui)
-			mWindowManager.RenderImGuiFrame();
+		mWindowManager.RenderImGuiFrame(show_gui);
 
 		glfwSwapBuffers(window);
 	}

src/EDAF80/assignment4.cpp
@@ -155,8 +155,7 @@ edaf80::Assignment4::run()
 
 		if (show_logs)
 			Log::View::Render();
-		if (show_gui)
-			mWindowManager.RenderImGuiFrame();
+		mWindowManager.RenderImGuiFrame(show_gui);
 
 		glfwSwapBuffers(window);
 	}

src/EDAF80/assignment5.cpp
@@ -132,8 +132,7 @@ edaf80::Assignment5::run()
 
 		if (show_logs)
 			Log::View::Render();
-		if (show_gui)
-			mWindowManager.RenderImGuiFrame();
+		mWindowManager.RenderImGuiFrame(show_gui);
 
 		glfwSwapBuffers(window);
 	}

src/EDAN35/assignment2.cpp
@@ -507,8 +507,7 @@ edan35::Assignment2::run()
 
 		if (show_logs)
 			Log::View::Render();
-		if (show_gui)
-			mWindowManager.RenderImGuiFrame();
+		mWindowManager.RenderImGuiFrame(show_gui);
 
 		glfwSwapBuffers(window);
 	}

src/core/WindowManager.cpp
@@ -208,10 +208,11 @@ void WindowManager::NewImGuiFrame()
 	ImGui::NewFrame();
 }
 
-void WindowManager::RenderImGuiFrame()
+void WindowManager::RenderImGuiFrame(bool show_gui)
 {
 	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	if (show_gui)
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void WindowManager::ToggleFullscreenStatusForWindow(GLFWwindow* const window) noexcept

src/core/WindowManager.hpp
@@ -39,7 +39,7 @@ public:
 	GLFWwindow* CreateGLFWWindow(std::string const& title, WindowDatum const& data, unsigned int msaa = 1u, bool fullscreen = false, bool resizable = false, SwapStrategy swap = SwapStrategy::enable_vsync);
 	void DestroyWindow(GLFWwindow* const window);
 	void NewImGuiFrame();
-	void RenderImGuiFrame();
+	void RenderImGuiFrame(bool show_gui);
 	void ToggleFullscreenStatusForWindow(GLFWwindow* const window) noexcept;
 
 private: