LUGGPublic/CG_Labs

commit c203e757fab46b00e8974589c0747fe9e9e5d84e

core: Add helpers to render an orthonormal basis
Fixes LU_Computer_Graphics_Labs#69 (internal GitLab)

src/EDAF80/assignment1.cpp
@@ -48,6 +48,8 @@ int main()
 		return EXIT_FAILURE;
 	}
 
+	bonobo::init();
+
 	//
 	// Load the sphere geometry
 	//
@@ -181,6 +183,7 @@ int main()
 	bool pause_animation = false;
 	bool show_logs = true;
 	bool show_gui = true;
+	bool show_basis = false;
 	float time_scale = 1.0f;
 
 	while (!glfwWindowShouldClose(window)) {
@@ -246,8 +249,8 @@ int main()
 		// TODO: Replace this explicit rendering of the Earth and Moon
 		// with a traversal of the scene graph and rendering of all its
 		// nodes.
-		earth.render(animation_delta_time_us, camera.GetWorldToClipMatrix(), glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 0.0f, 0.0f)));
-		//moon.render(animation_delta_time_us, camera.GetWorldToClipMatrix());
+		earth.render(animation_delta_time_us, camera.GetWorldToClipMatrix(), glm::translate(glm::mat4(1.0f), glm::vec3(2.0f, 0.0f, 0.0f)), show_basis);
+		//moon.render(animation_delta_time_us, camera.GetWorldToClipMatrix(), glm::mat4(1.0f), show_basis);
 
 
 		//
@@ -258,6 +261,8 @@ int main()
 		{
 			ImGui::Checkbox("Pause the animation", &pause_animation);
 			ImGui::SliderFloat("Time scale", &time_scale, 1e-1f, 10.0f);
+			ImGui::Separator();
+			ImGui::Checkbox("Show basis", &show_basis);
 		}
 		ImGui::End();
 
@@ -287,6 +292,8 @@ int main()
 	glDeleteTextures(1, &venus_texture);
 	glDeleteTextures(1, &mars_texture);
 	glDeleteTextures(1, &sun_texture);
+
+	bonobo::deinit();
 
 	return EXIT_SUCCESS;
 }

shaders/common/basis.frag
@@ -1,0 +1,26 @@
+#version 410
+
+in VS_OUT {
+	flat uint axis_index;
+} vs_out;
+
+out vec4 frag_color;
+
+void main()
+{
+	switch (vs_out.axis_index)
+	{
+	case 0:
+		frag_color = vec4(0.8f, 0.2f, 0.2f, 1.0f);
+		break;
+	case 1:
+		frag_color = vec4(0.2f, 0.8f, 0.2f, 1.0f);
+		break;
+	case 2:
+		frag_color = vec4(0.2f, 0.2f, 0.8f, 1.0f);
+		break;
+	default:
+		frag_color = vec4(1.0f);
+		break;
+	}
+}

shaders/common/basis.vert
@@ -1,0 +1,46 @@
+#version 410
+
+layout (location = 0) in vec3 vertex;
+
+uniform mat4 vertex_model_to_world;
+uniform mat4 vertex_world_to_clip;
+uniform float thickness_scale;
+uniform float length_scale;
+
+out VS_OUT {
+	flat uint axis_index;
+} vs_out;
+
+
+void main()
+{
+	vs_out.axis_index = gl_InstanceID;
+	vec3 local_vertex = vertex;
+
+	// One arrow is instanced three times, to provide all three axes. That
+	// arrow represents a (1 0 0) vector.
+
+	// Scale the arrow before it is transformed to represent an axis other
+	// than x.
+	local_vertex.yz *= thickness_scale;
+	local_vertex.x *= length_scale;
+
+	// The first instance is for the x axis, so no additional
+	// transformation is needed.
+	//
+	// The second instance is for the y axis, so swap the x and y
+	// components.
+	if (vs_out.axis_index == 1)
+	{
+		local_vertex.xy = local_vertex.yx;
+	}
+	// The third instance is for the z axis, so set x to -z, and z to x.
+	else if (vs_out.axis_index == 2)
+	{
+		float tmpX = local_vertex.x;
+		local_vertex.x = -local_vertex.z;
+		local_vertex.z = tmpX;
+	}
+
+	gl_Position = vertex_world_to_clip * vertex_model_to_world * vec4(local_vertex, 1.0);
+}

src/EDAF80/CelestialBody.cpp
@@ -3,6 +3,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/trigonometric.hpp>
 
+#include "core/helpers.hpp"
 #include "core/Log.h"
 
 CelestialBody::CelestialBody(bonobo::mesh_data const& shape,
@@ -16,7 +17,8 @@ CelestialBody::CelestialBody(bonobo::mesh_data const& shape,
 
 glm::mat4 CelestialBody::render(std::chrono::microseconds elapsed_time,
                                 glm::mat4 const& view_projection,
-                                glm::mat4 const& parent_transform)
+                                glm::mat4 const& parent_transform,
+                                bool show_basis)
 {
 	// Convert the duration from microseconds to seconds.
 	auto const elapsed_time_s = std::chrono::duration<float>(elapsed_time).count();
@@ -27,6 +29,11 @@ glm::mat4 CelestialBody::render(std::chrono::microseconds elapsed_time,
 	_body.spin.rotation_angle = -glm::half_pi<float>() / 2.0f;
 
 	glm::mat4 world = parent_transform;
+
+	if (show_basis)
+	{
+		bonobo::renderBasis(1.0f, 2.0f, view_projection, world);
+	}
 
 	// Note: The second argument of `node::render()` is supposed to be the
 	// parent transform of the node, not the whole world matrix, as the

src/EDAF80/CelestialBody.hpp
@@ -42,11 +42,14 @@ public:
 	//!             clip space
 	//! @param [in] parent_transform Matrix transforming from the parent’s
 	//!             local space to world space
+	//! @param [in] show_basis Show a 3D basis transformed by the world matrix
+	//!             of this celestial body
 	//! @return Matrix transforming from this celestial body’s local space
 	//!         to world space
 	glm::mat4 render(std::chrono::microseconds elapsed_time,
 	                 glm::mat4 const& view_projection,
-	                 glm::mat4 const& parent_transform = glm::mat4(1.0f));
+	                 glm::mat4 const& parent_transform = glm::mat4(1.0f),
+	                 bool show_basis = false);
 
 	//! \brief Mark another celestial body as being “attached” to the current one.
 	void add_child(CelestialBody* child);

src/EDAF80/assignment2.cpp
@@ -30,6 +30,13 @@ edaf80::Assignment2::Assignment2(WindowManager& windowManager) :
 	if (window == nullptr) {
 		throw std::runtime_error("Failed to get a window: aborting!");
 	}
+
+	bonobo::init();
+}
+
+edaf80::Assignment2::~Assignment2()
+{
+	bonobo::deinit();
 }
 
 void
@@ -157,6 +164,9 @@ edaf80::Assignment2::run()
 	auto polygon_mode = bonobo::polygon_mode_t::fill;
 	bool show_logs = true;
 	bool show_gui = true;
+	bool show_basis = false;
+	float basis_thickness_scale = 1.0f;
+	float basis_length_scale = 1.0f;
 
 	changeCullMode(cull_mode);
 
@@ -234,10 +244,16 @@ edaf80::Assignment2::run()
 			ImGui::Checkbox("Enable interpolation", &interpolate);
 			ImGui::Checkbox("Use linear interpolation", &use_linear);
 			ImGui::SliderFloat("Catmull-Rom tension", &catmull_rom_tension, 0.0f, 1.0f);
+			ImGui::Separator();
+			ImGui::Checkbox("Show basis", &show_basis);
+			ImGui::SliderFloat("Basis thickness scale", &basis_thickness_scale, 0.0f, 100.0f);
+			ImGui::SliderFloat("Basis length scale", &basis_length_scale, 0.0f, 100.0f);
 		}
 		ImGui::End();
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		if (show_basis)
+			bonobo::renderBasis(basis_thickness_scale, basis_length_scale, mCamera.GetWorldToClipMatrix());
 		if (show_logs)
 			Log::View::Render();
 		mWindowManager.RenderImGuiFrame(show_gui);

src/EDAF80/assignment2.hpp
@@ -23,7 +23,7 @@ namespace edaf80
 		//!
 		//! It will release the bonobo modules initialised by the
 		//! constructor, as well as the window.
-		~Assignment2() = default;
+		~Assignment2();
 
 		//! \brief Contains the logic of the assignment, along with the
 		//! render loop.

src/EDAF80/assignment3.cpp
@@ -30,6 +30,13 @@ edaf80::Assignment3::Assignment3(WindowManager& windowManager) :
 	if (window == nullptr) {
 		throw std::runtime_error("Failed to get a window: aborting!");
 	}
+
+	bonobo::init();
+}
+
+edaf80::Assignment3::~Assignment3()
+{
+	bonobo::deinit();
 }
 
 void
@@ -135,6 +142,9 @@ edaf80::Assignment3::run()
 	bool show_logs = true;
 	bool show_gui = true;
 	bool shader_reload_failed = false;
+	bool show_basis = false;
+	float basis_thickness_scale = 1.0f;
+	float basis_length_scale = 1.0f;
 
 	changeCullMode(cull_mode);
 
@@ -210,8 +220,15 @@ edaf80::Assignment3::run()
 			ImGui::ColorEdit3("Specular", glm::value_ptr(specular));
 			ImGui::SliderFloat("Shininess", &shininess, 1.0f, 1000.0f);
 			ImGui::SliderFloat3("Light Position", glm::value_ptr(light_position), -20.0f, 20.0f);
+			ImGui::Separator();
+			ImGui::Checkbox("Show basis", &show_basis);
+			ImGui::SliderFloat("Basis thickness scale", &basis_thickness_scale, 0.0f, 100.0f);
+			ImGui::SliderFloat("Basis length scale", &basis_length_scale, 0.0f, 100.0f);
 		}
 		ImGui::End();
+
+		if (show_basis)
+			bonobo::renderBasis(basis_thickness_scale, basis_length_scale, mCamera.GetWorldToClipMatrix());
 
 		opened = ImGui::Begin("Render Time", nullptr, ImGuiWindowFlags_None);
 		if (opened)

commit 8333be6555f9a61f85915bc772e9abdd8337a104

EDAF80/Lab1: Fix framework deinitialisation
Tear down of the log system is already performed by the destructor of
the `Bonobo` class, so there is no need to perform it manually which
could cause issues. Also `bonobo::deinit()` was not being called.

Fixes: c203e75 ("core: Add helpers to render an orthonormal basis")

src/EDAF80/assignment1.cpp
@@ -57,6 +57,8 @@ int main()
 	if (objects.empty()) {
 		LogError("Failed to load the sphere geometry: exiting.");
 
+		bonobo::deinit();
+
 		return EXIT_FAILURE;
 	}
 	bonobo::mesh_data const& sphere = objects.front();
@@ -75,8 +77,7 @@ int main()
 	if (celestial_body_shader == 0u) {
 		LogError("Failed to generate the “Celestial Body” shader program: exiting.");
 
-		Log::View::Destroy();
-		Log::Destroy();
+		bonobo::deinit();
 
 		return EXIT_FAILURE;
 	}
@@ -88,8 +89,7 @@ int main()
 	if (celestial_ring_shader == 0u) {
 		LogError("Failed to generate the “Celestial Ring” shader program: exiting.");
 
-		Log::View::Destroy();
-		Log::Destroy();
+		bonobo::deinit();
 
 		return EXIT_FAILURE;
 	}