4b50c697949dd5cdb0be22b589ed8c7bb6529f4c
Rikard Olajos <rikard.olajos@cs.lth.se>
2024-12-09 14:19:12 +0100

    resources: Fix specular normal and specular maps in Sponza

--- CMake/RetrieveResourceArchive.cmake
+++ CMake/RetrieveResourceArchive.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare (
 	resources
-	URL [[http://fileadmin.cs.lth.se/cs/Education/EDA221/assignments/EDAF80_resources.zip]]
-	URL_HASH [[SHA512=1ed9a167e4b06eaa7f0a000881ce7bc315cdc8fd38156cf66adc194f0922d1930ef7591275b37229a0261a84ed1c6869a1b3ac237a3cdca1e8b21d90f484dd11]]
+	URL [[http://fileadmin.cs.lth.se/cs/Education/EDAF80/assignments/resources.zip]]
+	URL_HASH [[SHA512=832b8e45973705b69eeae61be75dcc444949c53d8656c483664896b9e8daadce7f826b244b93bc258d0d3678721d4ab7d42bcaae02703590458ad25663d647cf]]
 	SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/res"
 )
 
